81b1da39496f1c6987568b790d2be4b103d0f6dc
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2019-06-17 23:57:45 -0300

    pacman animation finished

--- codigo/pyxel/pacman.py
+++ codigo/pyxel/pacman.py
@@ -1,5 +1,18 @@
 import pyxel
 
+
+#character state
+STOPPED = 0
+WALKING = 1
+
+
+#character orientation
+RIGHT = 0
+LEFT = 1
+DOWN = 2
+UP = 3
+INITIAL = 4
+
 #cell types
 CELL_A = (16,0)
 CELL_B = (24,0)
@@ -78,8 +91,27 @@ class GameMap:
 
 
 class Player:
-    def __init__(self, position, character_type):
+    def __init__(self, position, direction):
         self.position = position
+        self.direction = direction
+        self.current_animation_frame = 0
+        self.animation_frames = [
+            [(0,32),(8,32),(0,40),(8,40)],    #right
+            [(0,32),(32,32),(24,40),(32,40)], #left
+            [(0,32),(40,32),(48,32),(40,40)], #down
+            [(0,32),(16,32),(24,32),(16,40)]  #up
+        ]
+
+    def update(self):
+        if pyxel.frame_count % 3 == 0:
+            self.current_animation_frame = (self.current_animation_frame + 1) % 4
+
+    def draw(self):
+        if self.direction != INITIAL:
+            pyxel.blt(self.position[0], self.position[1], 0, 
+            self.animation_frames[self.direction][self.current_animation_frame][0], 
+            self.animation_frames[self.direction][self.current_animation_frame][1], 
+            8, 8, 0)
 
 class Ghost:
     def __init__(self, position, ghost_type):
@@ -88,23 +120,35 @@ class Ghost:
 
 class Game:
     def __init__(self):
-        pyxel.init(184, 200)
+        pyxel.init(184, 210, fps=60)
         pyxel.load('pacman.pyxel')
         #read map data from file
         with open('map1.txt') as f:
             map_data = f.readlines()
         map_data = [l.strip() for l in map_data]
         self.game_map = GameMap(map_data, 20)
-        self.is_map_drawn = False
 
+        self.character_direction = INITIAL
+        self.player = Player((30,202), self.character_direction)
         #never put code after this line, it will not be executed
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.character_direction = LEFT
+        elif pyxel.btn(pyxel.KEY_RIGHT):
+            self.character_direction = RIGHT
+        elif pyxel.btn(pyxel.KEY_DOWN):
+            self.character_direction = DOWN
+        elif pyxel.btn(pyxel.KEY_UP):
+            self.character_direction = UP
+        
+        self.player.direction = self.character_direction
+        self.player.update()
     
     def draw(self):
-        if not self.is_map_drawn:
-            self.game_map.draw()
+        pyxel.cls(0)
+        self.game_map.draw()
+        self.player.draw()
 
 Game()
