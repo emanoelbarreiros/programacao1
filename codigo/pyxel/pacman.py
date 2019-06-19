a971646780b0ca0e5527b04589c8fff073d5619b
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2019-06-18 21:44:56 -0300

    movement done and eating food

--- codigo/pyxel/pacman.py
+++ codigo/pyxel/pacman.py
@@ -68,7 +68,6 @@ CELL_TYPES = {
     '1': CELL_FOOD,
     '2': CELL_POWER,
     '3': CELL_DOOR
-
 }
 
 #BLINKY #red
@@ -92,22 +91,35 @@ class GameMap:
     def is_player_allowed(self, tile):
         if tile:
             #the cell is empty or has food or has power
-            x, y = tile
-            allowed = self.definition[y][x] == '0' or self.definition[y][x] == '1' or self.definition[y][x] == '2'
+            line, column = tile
+            allowed = self.definition[line][column] == '0' or self.definition[line][column] == '1' or self.definition[line][column] == '2'
             return allowed
         else:
             return False
 
+    def eat_food(self, line, column):
+        if self.definition[line][column] == '1'or self.definition[line][column] == '2': #food or power
+            self.definition[line] = self.definition[line][:column] + '0' + self.definition[line][column + 1:]
+
 class Player:
     def __init__(self, tile, current_direction, top_offset, game_map):
         self.intended_direction = current_direction
+        self.current_direction = current_direction
         self.state = STOPPED
         self.top_offset = top_offset
         self.tile = tile
         self.speed = 1
         self.game_map = game_map
-        self.absolute_position = (self.tile[0]*8, self.tile[1]*8 + self.top_offset)
+        self.steps_per_tile = self.game_map.tile_size // self.speed
+        self.steps = {
+            RIGHT: [(self.speed,0)]*self.steps_per_tile,
+            LEFT: [(-self.speed,0)]*self.steps_per_tile,
+            UP: [(0,-self.speed)]*self.steps_per_tile,
+            DOWN: [(0,self.speed)]*self.steps_per_tile
+        }
+        self.absolute_position = (self.tile[1]*8, self.tile[0]*8 + self.top_offset)
         self.current_animation_frame = 0
+        self.move_buffer = []
         self.animation_frames = [
             [(0,32),(8,32),(0,40),(8,40)],    #right
             [(0,32),(32,32),(24,40),(32,40)], #left
@@ -116,64 +128,70 @@ class Player:
             [(0,32),(0,32),(0,32),(0,32)]  #away
         ]
 
-    @property
-    def intended_direction(self):
-        return self._intended_direction
+    def get_current_tile(self):
+        tile = ((self.absolute_position[1] - self.top_offset) // 8, self.absolute_position[0] // 8)
+        return tile
 
-    @intended_direction.setter
-    def intended_direction(self, direction):
-        self._intended_direction = direction
-        self.state = WALKING #change this line so that it only walks if it is allowed
+    def opposites(self, dir1, dir2):
+        are_opposites = (dir1 == UP and dir2 == DOWN) or (dir1 == DOWN and dir2 == UP) or (dir1 == LEFT and dir2 == RIGHT) or (dir1 == RIGHT and dir2 == LEFT)
+        return are_opposites
 
-        if self.state == WALKING:
-            self.current_direction = self._intended_direction
-        
+    def handle_movement(self):
+        move = None
+        if self.opposites(self.intended_direction, self.current_direction):
+            count = len(self.move_buffer)
+            self.move_buffer.clear() #clear the move buffer
+            self.move_buffer.extend(self.steps[self.intended_direction][:self.steps_per_tile - count]) #make the character return to the tile base position
+            self.current_direction = self.intended_direction
+
+        if len(self.move_buffer) == 0: #will try to move to the next tile
+            #calculate tile the character is at, based on absolute position
+            line, column = self.get_current_tile()
+            intended_tile = None
+            steps = None
+            #calculate the tile the character wants to go to
+            if self.intended_direction == UP:
+                intended_tile = (line - 1, column)
+            elif self.intended_direction == DOWN:
+                intended_tile = (line + 1, column)
+            elif self.intended_direction == LEFT:
+                intended_tile = (line, column - 1)
+            elif self.intended_direction == RIGHT:
+                intended_tile = (line, column + 1)
+
+            if self.game_map.is_player_allowed(intended_tile): #if player is allowed to move based on the intended direction
+                self.current_direction = self.intended_direction
+                self.move_buffer.extend(self.steps[self.current_direction])
+                move = self.move_buffer.pop(0)
+            else: #else, try to move based on the old direction
+                if self.current_direction == UP:
+                    intended_tile = (line - 1, column)
+                elif self.current_direction == DOWN:
+                    intended_tile = (line + 1, column)
+                elif self.current_direction == LEFT:
+                    intended_tile = (line, column - 1)
+                elif self.current_direction == RIGHT:
+                    intended_tile = (line, column + 1)
+
+                if self.game_map.is_player_allowed(intended_tile): #if allowed to move using the old direction
+                    self.move_buffer.extend(self.steps[self.current_direction])
+                    move = self.move_buffer.pop(0)
+
+        else:
+            move = self.move_buffer.pop(0)
+
+        if move:
+            self.absolute_position = (self.absolute_position[0] + move[0], self.absolute_position[1] + move[1])
+
+    def handle_food(self):
+        tile = self.get_current_tile()
+        base_tile_position = (tile[1]*self.game_map.tile_size, tile[0]*self.game_map.tile_size + self.top_offset)
+        if self.absolute_position == base_tile_position:
+            self.game_map.eat_food(*tile)
 
     def update(self):
-        #calculate tile the character is at based on absolute position
-        tile = (self.absolute_position[0] // 8, (self.absolute_position[1] - self.top_offset) // 8)
-
-        #try to move the character
-        #first decide if the character will change direction or will continue moving using the current direction
-        intended_tile = None
-        intended_movement = None
-        x_movement = 0
-        y_movement = 0
-        if self.intended_direction == UP:
-            intended_tile = (self.absolute_position[0] // 8, (self.absolute_position[1] - self.top_offset - self.speed) // 8)
-            y_movement = -self.speed
-        elif self.intended_direction == DOWN:
-            intended_tile = (self.absolute_position[0] // 8, (self.absolute_position[1] - self.top_offset + self.speed + 8) // 8)
-            y_movement = self.speed
-        elif self.intended_direction == LEFT:
-            intended_tile = ((self.absolute_position[0] - self.speed) // 8, (self.absolute_position[1] - self.top_offset) // 8)
-            x_movement = -self.speed
-        elif self.intended_direction == RIGHT:
-            intended_tile = ((self.absolute_position[0] + self.speed + 8) // 8, (self.absolute_position[1] - self.top_offset) // 8)
-            x_movement = self.speed
-
-        #check if change of tiles is allowed
-        if intended_tile and self.game_map.is_player_allowed(intended_tile):
-            self.current_direction = self.intended_direction
-        
-        #check if current direction is allowed
-        if self.current_direction != self.intended_direction:
-            if self.current_direction == UP:
-                intended_tile = (self.absolute_position[0] // 8, (self.absolute_position[1] - self.top_offset - self.speed) // 8)
-                y_movement = -self.speed
-            elif self.current_direction == DOWN:
-                intended_tile = (self.absolute_position[0] // 8, (self.absolute_position[1] - self.top_offset + self.speed + 8) // 8)
-                y_movement = self.speed
-            elif self.current_direction == LEFT:
-                intended_tile = ((self.absolute_position[0] - self.speed) // 8, (self.absolute_position[1] - self.top_offset) // 8)
-                x_movement = -self.speed
-            elif self.current_direction == RIGHT:
-                intended_tile = ((self.absolute_position[0] + self.speed) // 8, (self.absolute_position[1] - self.top_offset + 8) // 8)
-                x_movement = self.speed
-        
-        if self.game_map.is_player_allowed(intended_tile):
-            x, y = self.absolute_position
-            self.absolute_position = (x + x_movement, y + y_movement)
+        self.handle_movement()
+        self.handle_food()
 
     def draw(self):
         if pyxel.frame_count % 3 == 0 and self.state == WALKING:
@@ -201,7 +219,7 @@ class Game:
         map_data = [l.strip() for l in map_data]
         self.game_map = GameMap(map_data, self.top_offset)
         self.intended_direction = AWAY
-        self.player_tile = (11,12)
+        self.player_tile = (12,11)
         self.player = Player(self.player_tile, self.intended_direction, self.top_offset, self.game_map)
         #never put code after this line, it will not be executed
         pyxel.run(self.update, self.draw)
@@ -209,12 +227,16 @@ class Game:
     def update(self):
         if pyxel.btn(pyxel.KEY_LEFT):
             self.intended_direction = LEFT
+            self.player.state = WALKING
         elif pyxel.btn(pyxel.KEY_RIGHT):
             self.intended_direction = RIGHT
+            self.player.state = WALKING
         elif pyxel.btn(pyxel.KEY_DOWN):
             self.intended_direction = DOWN
+            self.player.state = WALKING
         elif pyxel.btn(pyxel.KEY_UP):
             self.intended_direction = UP
+            self.player.state = WALKING
         
         self.player.intended_direction = self.intended_direction
         self.player.update()
